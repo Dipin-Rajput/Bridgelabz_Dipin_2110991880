bcb1e930834ceba5e20bd17685b7bfb80c256007
Dipin-Rajput India <dipinrajput707@gmail.com>
2024-12-20 10:38:32 +0530

    [Dipin]Update: Updated ExceptionHandling.py

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -60,3 +60,70 @@ except ZeroDivisionError:
     print("You are trying to divide by 0.")
 except NameError:
     print("Variable not defined.")
+print()
+
+# Try with else clause
+
+# In Python, we can also use the else clause on the try-except block which must be present after all the except clauses. The code enters the else block only if the try clause does not raise an exception.
+
+def check(a, b):
+
+    try:
+        print((a+b)/(a-b))
+    except:
+        print("Error found.")
+    else:
+        print("No error found.")
+
+check(2, 3) # Else statement will run, since no exception found.
+check(3, 3) # except statement will run, since exception found.
+print()
+
+# Finally Keyword in Python
+
+# Python provides a keyword finally, which is always executed after the try and except blocks. The final block always executes after the normal termination of the try block or after the try block terminates due to some exception. The code within the finally block is always executed.
+
+def opr(a, c):
+    
+    try:
+        b = a/(a-c)
+        print(b)
+    except ZeroDivisionError:
+        print("You are dividing by 0.")
+    else:
+        print("No error found.")
+    finally:
+        print("This statement will always print.")
+
+opr(4, 4)
+opr(5, 4)
+print()
+
+# Raise Exception
+
+# The raise statement allows the programmer to force a specific exception to occur.
+
+try:
+    age = int(input("Enter your age: "))
+    if(age >= 18 and age <= 24):
+        print("You are eligible.")
+    else:
+        raise Exception("Over age.") # This will raise the exception in case age is out of limit.
+        
+except:
+    print("You are not eligible")
+print()
+
+# Now to print exception we get.
+
+try:
+    age = int(input("Enter your age: "))
+    if(age >= 18 and age <= 24):
+        print("You are eligible.")
+    else:
+        raise Exception("Over age.") # This will raise the exception in case age is out of limit.
+        
+except Exception as e: # This will print the exception.
+    print("You are not eligible:", e)
+
+
