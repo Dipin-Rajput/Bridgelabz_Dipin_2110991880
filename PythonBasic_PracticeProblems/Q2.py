61cf4fda78c3fe2e975f5f61d3055754c5e84b14
Dipin-Rajput India <dipinrajput707@gmail.com>
2024-12-31 22:43:42 +0530

    [Dipin]Delete: Deleted folder PythonBasic_PracticeProblems

--- PythonBasic_PracticeProblems/Q2.py
+++
@@ -1,5 +0,0 @@
-# 2) Print the Sum of a Current Number and a Previous number: Write a Python code to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
-
-print("Sum of Current and Previous numbers:", end = " ")
-for i in range(1, 11):
-    print(i + i - 1, end = " ")
--- PythonBasic_PracticeProblems/Q4.py
+++
@@ -1,7 +0,0 @@
-# 4) Remove first n characters from a string: Write a Python code to remove characters from a string from 0 to n and return a new string.
-
-str = input("Enter your string: ")
-n = int(input("Enter number of character you want to remove: "))
-
-result = str[n:]
-print("Your resulting string is:", result)
