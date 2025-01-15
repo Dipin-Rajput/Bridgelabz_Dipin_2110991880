9e60d0ba24e962b683c858c84798733b094e0540
Dipin-Rajput India <dipinrajput707@gmail.com>
2025-01-15 13:20:49 +0530

    [Dipin]Update: Updated Stack.py file

--- Stack.py
+++ Stack.py
@@ -1,5 +1,21 @@
 # Stack
 
+#  A stack is a linear data structure that stores items in a Last-In/First-Out (LIFO) or First-In/Last-Out (FILO) manner.
+# In stack, a new element is added at one end and an element is removed from that end only. The insert and delete operations are often called push and pop.
+
+# The functions associated with stack are:
+
+# empty() – Returns whether the stack is empty – Time Complexity: O(1)
+# size() – Returns the size of the stack – Time Complexity: O(1)
+# top() / peek() – Returns a reference to the topmost element of the stack – Time Complexity: O(1)
+# push(a) – Inserts the element ‘a’ at the top of the stack – Time Complexity: O(1)
+# pop() – Deletes the topmost element of the stack – Time Complexity: O(1)
+
+# Implementation of stack can be done in various ways:
+
+# 1. list
+# 2. Collection module
+
 # Stack Implementation using list
 
 class Stack:
@@ -43,6 +59,11 @@ stack.display()
 print("top:", stack.peek())
 print("empty?", stack.isempty())
 print("size:", stack.size())
+
+# The biggest issue is that it can run into speed issues as it grows. The items in the list are stored next to each other in memory, if the stack grows bigger than the block of memory that currently holds it, then Python needs to do some memory allocations. This can lead to some append() calls taking much longer than other ones.
+
+
+# Python stack can be implemented using the deque class from the collections module. Deque is preferred over the list in the cases where we need quicker append and pop operations from both the ends of the container, as deque provides an O(1) time complexity for append and pop operations as compared to list which provides O(n) time complexity.
 
 # Stack Implementation using Collection Module
 
@@ -89,3 +110,50 @@ print(stack.pop())
 print("---->>>>", stack.display())
 print(stack.peek())
 print(stack.size())
+
+
+# Balanced Parentheses
+
+str = "(()(())())"
+stack = []
+
+for i in str:
+
+    if(not stack):
+        stack.append(i)
+        continue
+
+    if(i == "("):
+        stack.append(i)
+    else:
+        if(stack[-1] == "("):
+            stack.pop()
+
+if(len(stack) == 0):
+    print("String has Balanced Parentheses:", str)
+
+else:
+    print("String doesn't has Balanced Parentheses:", str)
+
+
+# Evaluate Postfix Expression using stack.
+
+str = "53+82-*"
+stack = []
+
+for i in str:
+    if(i.isdigit()):
+        stack.append(i)
+
+    else:
+        val1 = int(stack.pop())
+        val2 = int(stack.pop())
+
+        match i:
+
+            case "+": stack.append(val1 + val2)
+            case "-": stack.append(val1 - val2)
+            case "*": stack.append(val1 * val2)
+            case "/": stack.append(int(val1 / val2))
+
+print("Evaluation of Postfix Expression is:", stack.pop())
